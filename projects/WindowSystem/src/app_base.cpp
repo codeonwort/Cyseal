4874340fdd970c371ac1c92d9916485040a376b9
codeonwort <codeonwort@gmail.com>
2021-09-15 22:55:19 +0900

    Cleanup broken comments

--- projects/WindowSystem/src/app_base.cpp
+++ projects/WindowSystem/src/app_base.cpp
@@ -1,8 +1,6 @@
 #include "app_base.h"
 
-// Forward declarations
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-//INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 AppBase::AppBase()
 {
@@ -11,7 +9,7 @@ AppBase::AppBase()
 	width = 800; height = 600;
 	title = L"title here";
 
-	// according to msdn this is a fixed value. so don't query it every time...
+	// According to MSDN this is a fixed value, so don't query it every time.
 	QueryPerformanceFrequency(&time_freq);
 	elapsedSecondsFromStart = 0.0f;
 }
@@ -108,12 +106,6 @@ void AppBase::run(HINSTANCE hInst_, int nCmdShow_, std::wstring windowClass_)
 	onTerminate();
 }
 
-
-//
-//  함수: MyRegisterClass()
-//
-//  목적: 창 클래스를 등록합니다.
-//
 ATOM AppBase::MyRegisterClass(HINSTANCE hInstance)
 {
 	WNDCLASSEXW wcex;
@@ -162,35 +154,13 @@ BOOL AppBase::InitInstance(HINSTANCE hInstance, int nCmdShow_)
 	return TRUE;
 }
 
-//
-//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  목적:  주 창의 메시지를 처리합니다.
-//
-//  WM_COMMAND  - 응용 프로그램 메뉴를 처리합니다.
-//  WM_PAINT    - 주 창을 그립니다.
-//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
-//
-//
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)
 	{
 	case WM_COMMAND:
 	{
-		//int wmId = LOWORD(wParam);
-		// 메뉴 선택을 구문 분석합니다.
-		//switch (wmId)
-		//{
-		/*case IDM_ABOUT:
-			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-			break;
-		case IDM_EXIT:
-			DestroyWindow(hWnd);
-			break;*/
-		//default:
-			return DefWindowProcW(hWnd, message, wParam, lParam);
-		//}
+		return DefWindowProcW(hWnd, message, wParam, lParam);
 	}
 	break;
 	case WM_KEYDOWN:
@@ -206,7 +176,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		PAINTSTRUCT ps;
 		HDC hdc = BeginPaint(hWnd, &ps);
 		static_cast<void>(hdc);
-		// TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다.
 		EndPaint(hWnd, &ps);
 	}
 	break;
@@ -218,23 +187,3 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	}
 	return 0;
 }
-
-// 정보 대화 상자의 메시지 처리기입니다.
-/*INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	UNREFERENCED_PARAMETER(lParam);
-	switch (message)
-	{
-	case WM_INITDIALOG:
-		return (INT_PTR)TRUE;
-
-	case WM_COMMAND:
-		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-		{
-			EndDialog(hDlg, LOWORD(wParam));
-			return (INT_PTR)TRUE;
-		}
-		break;
-	}
-	return (INT_PTR)FALSE;
-}*/
