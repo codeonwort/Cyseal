b379c666e276c9789f83fd1fb904d25731f92cb2
codeonwort <codeonwort@gmail.com>
2022-11-08 00:50:38 +0900

    ACES tone mapper

--- shaders/tone_mapping.hlsl
+++ shaders/tone_mapping.hlsl
@@ -3,6 +3,45 @@
 #define GAMMA_CORRECTION 2.2
 #define EXPOSURE         1.0
 
+// https://github.com/TheRealMJP/BakingLab/blob/master/BakingLab/ACES.hlsl
+// sRGB => XYZ => D65_2_D60 => AP1 => RRT_SAT
+static const float3x3 ACESInputMat =
+{
+    {0.59719, 0.35458, 0.04823},
+    {0.07600, 0.90834, 0.01566},
+    {0.02840, 0.13383, 0.83777}
+};
+
+// ODT_SAT => XYZ => D60_2_D65 => sRGB
+static const float3x3 ACESOutputMat =
+{
+    { 1.60475, -0.53108, -0.07367},
+    {-0.10208,  1.10813, -0.00605},
+    {-0.00327, -0.07276,  1.07602}
+};
+
+float3 RRTAndODTFit(float3 v)
+{
+    float3 a = v * (v + 0.0245786f) - 0.000090537f;
+    float3 b = v * (0.983729f * v + 0.4329510f) + 0.238081f;
+    return a / b;
+}
+
+float3 ACESFitted(float3 color)
+{
+    color = mul(ACESInputMat, color);
+
+    // Apply RRT and ODT
+    color = RRTAndODTFit(color);
+
+    color = mul(ACESOutputMat, color);
+
+    // Clamp to [0, 1]
+    color = saturate(color);
+
+    return color;
+}
+
 // ------------------------------------------------------------------------
 // Resource bindings
 
@@ -40,7 +79,10 @@ float4 mainPS(Interpolants interpolants) : SV_TARGET
     float4 color = sceneColor.SampleLevel(sceneColorSampler, screenUV, 0.0);
 
     // Reinhard tone mapper
-    color.rgb = float3(1.0, 1.0, 1.0) - exp(-color.rgb * EXPOSURE);
+    //color.rgb = float3(1.0, 1.0, 1.0) - exp(-color.rgb * EXPOSURE);
+
+    // ACES tone mapper
+    color.rgb = ACESFitted(color.rgb);
 
     // Gamma correction
     float gamma = 1.0 / GAMMA_CORRECTION;
