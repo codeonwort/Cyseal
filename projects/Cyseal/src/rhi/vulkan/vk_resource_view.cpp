ec42865beaf5f6d29b794916411924ad754a7d48
codeonwort <codeonwort@gmail.com>
2024-05-05 13:57:20 +0900

    Add missing COMPILE_BACKEND_VULKAN

--- projects/Cyseal/src/rhi/vulkan/vk_resource_view.cpp
+++ projects/Cyseal/src/rhi/vulkan/vk_resource_view.cpp
@@ -1,7 +1,11 @@
 #include "vk_resource_view.h"
 
+#if COMPILE_BACKEND_VULKAN
+
 void VulkanConstantBufferView::writeToGPU(RenderCommandList* commandList, void* srcData, uint32 sizeInBytes)
 {
 	// #wip-buffer: VulkanConstantBufferView::writeToGPU
 	CHECK_NO_ENTRY();
 }
+
+#endif // COMPILE_BACKEND_VULKAN
