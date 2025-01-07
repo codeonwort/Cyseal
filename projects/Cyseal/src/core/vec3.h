4d5de101a7420807f30fc9b19fb4276fe2784f6e
codeonwort <codeonwort@gmail.com>
2025-01-07 23:35:40 +0900

    Fix const correctness of vec3 equality operators

--- projects/Cyseal/src/core/vec3.h
+++ projects/Cyseal/src/core/vec3.h
@@ -21,8 +21,8 @@ public:
 		z = e2;
 	}
 
-	inline bool operator==(const vec3& v2) { return x == v2.x && y == v2.y && z == v2.z; }
-	inline bool operator!=(const vec3& v2) { return !(*this == v2); }
+	inline bool operator==(const vec3& v2) const { return x == v2.x && y == v2.y && z == v2.z; }
+	inline bool operator!=(const vec3& v2) const { return !(*this == v2); }
 
 	inline const vec3& operator+() const { return *this; }
 	inline vec3 operator-() const { return vec3(-x, -y, -z); }
