d0a5d4ee4fef3d5dac333ec935a67a48695f7c52
codeonwort <codeonwort@gmail.com>
2025-08-13 22:51:37 +0900

    Stub code for console app

---
+++ projects/Demo_ConsoleApp/src/main_ConsoleApp.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+#include "core/console_application.h"
+#include "rhi/dx12/d3d_device.h"
+
+// Demo project to test headless app (console-only, no swapchains).
+
+class DemoConsoleApp : public ConsoleApplication
+{
+protected:
+    virtual void onExecute() override
+    {
+        RenderDeviceCreateParams createParams{
+			.nativeWindowHandle  = NULL,
+			.bHeadless           = true,
+			.rawAPI              = ERenderDeviceRawAPI::DirectX12,
+			.raytracingTier      = ERaytracingTier::MaxTier,
+			.vrsTier             = EVariableShadingRateTier::MaxTier,
+			.meshShaderTier      = EMeshShaderTier::MaxTier,
+			.samplerFeedbackTier = ESamplerFeedbackTier::MaxTier,
+			.enableDebugLayer    = true,
+			.bDoubleBuffering    = false,
+			.windowType          = EWindowType::WINDOWED,
+			.windowWidth         = 1920,
+			.windowHeight        = 1080,
+		};
+
+		RenderDevice* renderDevice = new D3DDevice;
+		renderDevice->initialize(createParams);
+
+		// #todo-console: Render something.
+		// ...
+
+		renderDevice->destroy();
+		delete renderDevice;
+    }
+};
+
+int main()
+{
+    ApplicationCreateParams createParams{
+        .nativeWindowHandle = NULL,
+        .applicationName = L"StudyDirectX12",
+    };
+
+    DemoConsoleApp app;
+    EApplicationReturnCode ret = app.launch(createParams);
+
+    return 0;
+}
