20510ac04f0392f66d8a81ae5dc193311f9d0aa5
codeonwort <codeonwort@gmail.com>
2025-09-20 15:11:16 +0900

    Dont't use array in calculateWorldBounds()
    ... and fix tab inconsistencies done by this stupid Adaptive Formatting
    feature of Visual Studio :(

--- shaders/gpu_culling.hlsl
+++ shaders/gpu_culling.hlsl
@@ -6,7 +6,7 @@
 
 struct PushConstants
 {
-    uint numDrawCommands;
+	uint numDrawCommands;
 };
 
 // #todo-cull: Specific to base pass
@@ -31,79 +31,76 @@ RWBuffer<uint> drawCounterBuffer;
 
 AABB calculateWorldBounds(float3 localMin, float3 localMax, float4x4 localToWorld)
 {
-    float3 vs[8];
-    for (uint i = 0; i < 8; ++i)
-    {
-        vs[i].x = (i & 1) ? localMin.x : localMax.x;
-        vs[i].y = ((i >> 1) & 1) ? localMin.y : localMax.y;
-        vs[i].z = ((i >> 2) & 1) ? localMin.z : localMax.z;
-    }
-    float3 worldMin = FLT_MAX;
-    float3 worldMax = -FLT_MAX;
-    for (uint i = 0; i < 8; ++i)
-    {
-        vs[i] = mul(float4(vs[i], 1.0), localToWorld).xyz;
-        worldMin = min(worldMin, vs[i]);
-        worldMax = max(worldMax, vs[i]);
-    }
-    AABB ret;
-    ret.minBounds = worldMin;
-    ret.maxBounds = worldMax;
-    return ret;
+	float3 worldMin = FLT_MAX;
+	float3 worldMax = -FLT_MAX;
+	for (uint i = 0; i < 8; ++i)
+	{
+		float3 v;
+		v.x = (i & 1) ? localMin.x : localMax.x;
+		v.y = ((i >> 1) & 1) ? localMin.y : localMax.y;
+		v.z = ((i >> 2) & 1) ? localMin.z : localMax.z;
+		v = mul(float4(v, 1.0), localToWorld).xyz;
+		worldMin = min(worldMin, v);
+		worldMax = max(worldMax, v);
+	}
+	AABB ret;
+	ret.minBounds = worldMin;
+	ret.maxBounds = worldMax;
+	return ret;
 }
 
 // true if AABB is on positive side of the plane.
 bool hitTest_AABB_plane(AABB box, Plane3D plane)
 {
-    float3 boxCenter = 0.5 * (box.maxBounds + box.minBounds);
-    float3 boxHalfSize = 0.5 * (box.maxBounds - box.minBounds);
-    float r = dot(boxHalfSize, abs(plane.normal));
-    float s = dot(boxCenter, plane.normal) - plane.distance;
-    return -r <= s;
+	float3 boxCenter = 0.5 * (box.maxBounds + box.minBounds);
+	float3 boxHalfSize = 0.5 * (box.maxBounds - box.minBounds);
+	float r = dot(boxHalfSize, abs(plane.normal));
+	float s = dot(boxCenter, plane.normal) - plane.distance;
+	return -r <= s;
 }
 // true if AABB is inside of the frustum.
 bool hitTest_AABB_frustum(AABB box, Frustum3D frustum)
 {
-    for (int i = 0; i < 6; ++i)
-    {
-        if (!hitTest_AABB_plane(box, frustum.planes[i]))
-        {
-            return false;
-        }
-    }
-    return true;
+	for (int i = 0; i < 6; ++i)
+	{
+		if (!hitTest_AABB_plane(box, frustum.planes[i]))
+		{
+			return false;
+		}
+	}
+	return true;
 }
 // Same as hitTest_AABB_frustum(), but ignore the far plane.
 bool hitTest_AABB_frustumNoFarPlane(AABB box, Frustum3D frustum)
 {
-    for (int i = 0; i < 5; ++i)
-    {
-        if (!hitTest_AABB_plane(box, frustum.planes[i]))
-        {
-            return false;
-        }
-    }
-    return true;
+	for (int i = 0; i < 5; ++i)
+	{
+		if (!hitTest_AABB_plane(box, frustum.planes[i]))
+		{
+			return false;
+		}
+	}
+	return true;
 }
 
 [numthreads(1, 1, 1)]
 void mainCS(uint3 tid : SV_DispatchThreadID)
 {
-    uint objectID = drawCommandBuffer[tid.x].sceneItemIndex;
-    GPUSceneItem sceneItem = gpuSceneBuffer[objectID];
+	uint objectID = drawCommandBuffer[tid.x].sceneItemIndex;
+	GPUSceneItem sceneItem = gpuSceneBuffer[objectID];
 
-    AABB worldBounds = calculateWorldBounds(
-        sceneItem.localMinBounds,
-        sceneItem.localMaxBounds,
-        sceneItem.localToWorld);
-    
-    bool bInFrustum = hitTest_AABB_frustum(worldBounds, sceneUniform.cameraFrustum);
+	AABB worldBounds = calculateWorldBounds(
+		sceneItem.localMinBounds,
+		sceneItem.localMaxBounds,
+		sceneItem.localToWorld);
+	
+	bool bInFrustum = hitTest_AABB_frustum(worldBounds, sceneUniform.cameraFrustum);
 
-    if (bInFrustum)
-    {
-        uint nextItemIndex;
-        InterlockedAdd(drawCounterBuffer[0], 1, nextItemIndex);
+	if (bInFrustum)
+	{
+		uint nextItemIndex;
+		InterlockedAdd(drawCounterBuffer[0], 1, nextItemIndex);
 
-        culledDrawCommandBuffer[nextItemIndex] = drawCommandBuffer[tid.x];
-    }
+		culledDrawCommandBuffer[nextItemIndex] = drawCommandBuffer[tid.x];
+	}
 }
