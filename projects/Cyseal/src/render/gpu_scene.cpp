d2970fb24021e51e8688266668d11ba7d46dcc30
codeonwort <codeonwort@gmail.com>
2025-09-28 12:26:33 +0900

    Refactor GPUScene

--- projects/Cyseal/src/render/gpu_scene.cpp
+++ projects/Cyseal/src/render/gpu_scene.cpp
@@ -260,12 +260,21 @@ void GPUScene::renderGPUScene(RenderCommandList* commandList, uint32 swapchainIn
 		}
 		SCOPED_DRAW_EVENT_STRING(commandList, eventString);
 
+		Buffer::UploadBarrier uploadBarrier{
+			.syncBefore   = EBarrierSync::ALL,
+			.syncAfter    = EBarrierSync::COMPUTE_SHADING,
+			.accessBefore = EBarrierAccess::COMMON,
+			.accessAfter  = EBarrierAccess::SHADER_RESOURCE,
+		};
 		gpuSceneCommandBuffer[swapchainIndex]->singleWriteToGPU(
 			commandList,
 			sceneCommands.data(),
 			(uint32)(sizeof(GPUSceneCommand) * numSceneCommands),
-			0);
+			0,
+			uploadBarrier);
 
+		// #wip
+#if 0
 		BufferMemoryBarrier barriersBefore[] = {
 			{
 				EBufferMemoryLayout::COMMON,
@@ -279,6 +288,14 @@ void GPUScene::renderGPUScene(RenderCommandList* commandList, uint32 swapchainIn
 			},
 		};
 		commandList->resourceBarriers(_countof(barriersBefore), barriersBefore, 0, nullptr);
+#else
+		BufferBarrier barriersBefore[] = {
+			// No need for gpuSceneCommandBuffer due to custom uploadBarrier.
+			//{ EBarrierSync::ALL, EBarrierSync::COMPUTE_SHADING, EBarrierAccess::COMMON, EBarrierAccess::SHADER_RESOURCE, gpuSceneCommandBuffer.at(swapchainIndex) },
+			{ EBarrierSync::ALL, EBarrierSync::COMPUTE_SHADING, EBarrierAccess::COMMON, EBarrierAccess::UNORDERED_ACCESS, gpuSceneBuffer.get() },
+		};
+		commandList->barrier(_countof(barriersBefore), barriersBefore, 0, nullptr, 0, nullptr);
+#endif
 
 		ShaderParameterTable SPT{};
 		SPT.pushConstant("pushConstants", numSceneCommands);
@@ -289,6 +306,8 @@ void GPUScene::renderGPUScene(RenderCommandList* commandList, uint32 swapchainIn
 		commandList->bindComputeShaderParameters(pipelineState.get(), &SPT, volatileViewHeap.at(swapchainIndex));
 		commandList->dispatchCompute(numSceneCommands, 1, 1);
 
+		// #wip
+#if 0
 		BufferMemoryBarrier barriersAfter[] = {
 			{
 				EBufferMemoryLayout::UNORDERED_ACCESS,
@@ -297,6 +316,12 @@ void GPUScene::renderGPUScene(RenderCommandList* commandList, uint32 swapchainIn
 			},
 		};
 		commandList->resourceBarriers(_countof(barriersAfter), barriersAfter, 0, nullptr);
+#else
+		BufferBarrier barriersAfter[] = {
+			{ EBarrierSync::COMPUTE_SHADING, EBarrierSync::PIXEL_SHADING, EBarrierAccess::UNORDERED_ACCESS, EBarrierAccess::SHADER_RESOURCE, gpuSceneBuffer.get() },
+		};
+		commandList->barrier(_countof(barriersAfter), barriersAfter, 0, nullptr, 0, nullptr);
+#endif
 	}
 }
 
