d1290f0b8a05b10744ddf6b609bd04e21bdf4673
codeonwort <codeonwort@gmail.com>
2025-12-05 22:55:25 +0900

    Fix adaptive formatting :(

--- shaders/buffer_visualization.hlsl
+++ shaders/buffer_visualization.hlsl
@@ -19,7 +19,7 @@
 
 struct PushConstants
 {
-    uint modeEnum;
+	uint modeEnum;
 	uint width;
 	uint height;
 };
@@ -43,19 +43,19 @@ SamplerState textureSampler                 : register(s0, space0);
 
 struct Interpolants
 {
-    float4 posH : SV_POSITION;
-    float2 uv : TEXCOORD0;
+	float4 posH : SV_POSITION;
+	float2 uv : TEXCOORD0;
 };
 
 Interpolants mainVS(uint vertexID: SV_VertexID)
 {
-    Interpolants output;
+	Interpolants output;
 
-    output.uv = float2((vertexID << 1) & 2, vertexID & 2);
-    output.uv.y = 1.0 - output.uv.y;
-    output.posH = float4(output.uv * 2.0 + -1.0, 0.0, 1.0);
+	output.uv = float2((vertexID << 1) & 2, vertexID & 2);
+	output.uv.y = 1.0 - output.uv.y;
+	output.posH = float4(output.uv * 2.0 + -1.0, 0.0, 1.0);
 
-    return output;
+	return output;
 }
 
 // ------------------------------------------------------------------------
@@ -63,61 +63,61 @@ Interpolants mainVS(uint vertexID: SV_VertexID)
 
 float4 mainPS(Interpolants interpolants) : SV_TARGET
 {
-    float2 screenUV = interpolants.uv;
-    screenUV.y = 1.0 - screenUV.y;
+	float2 screenUV = interpolants.uv;
+	screenUV.y = 1.0 - screenUV.y;
 
-    uint modeEnum = pushConstants.modeEnum;
-    float4 color = float4(0.0, 0.0, 0.0, 1.0);
+	uint modeEnum = pushConstants.modeEnum;
+	float4 color = float4(0.0, 0.0, 0.0, 1.0);
 
-    GBUFFER0_DATATYPE encodedGBuffer0 = gbuffer0.Load(int3(interpolants.posH.xy, 0));
-    GBUFFER1_DATATYPE encodedGBuffer1 = gbuffer1.Load(int3(interpolants.posH.xy, 0));
-    GBufferData gbufferData = decodeGBuffers(encodedGBuffer0, encodedGBuffer1);
+	GBUFFER0_DATATYPE encodedGBuffer0 = gbuffer0.Load(int3(interpolants.posH.xy, 0));
+	GBUFFER1_DATATYPE encodedGBuffer1 = gbuffer1.Load(int3(interpolants.posH.xy, 0));
+	GBufferData gbufferData = decodeGBuffers(encodedGBuffer0, encodedGBuffer1);
 
-    if (modeEnum == MODE_MATERIAL_ID)
-    {
-        uint id = gbufferData.materialID;
-        float x = float((id * (id + 17) * (id + 15)) & 0xFF) / 255.0;
-        float y = float(((id + 4) * (id + 13) * (id + 11)) & 0xFF) / 255.0;
-        float z = float(((id * 5) * (id + 21) * (id + 19)) & 0xFF) / 255.0;
-        color.rgb = float3(x, y, z);
-    }
-    else if (modeEnum == MODE_ALBEDO)
-    {
-        color.rgb = gbufferData.albedo;
-    }
-    else if (modeEnum == MODE_ROUGHNESS)
-    {
-        color.rgb = gbufferData.roughness.xxx;
-    }
-    else if (modeEnum == MODE_METAL_MASK)
-    {
-        color.rgb = gbufferData.metalMask.xxx;
-    }
-    else if (modeEnum == MODE_NORMAL)
-    {
-        color.rgb = 0.5 * (1.0 + gbufferData.normalWS);
-    }
-    else if (modeEnum == MODE_DIRECT_LIGHTING)
-    {
-        color.rgb = sceneColor.SampleLevel(textureSampler, screenUV, 0.0).rgb;
-    }
-    else if (modeEnum == MODE_RAY_TRACED_SHADOWS)
-    {
-        color.rgb = shadowMask.SampleLevel(textureSampler, screenUV, 0.0).rrr;
-    }
-    else if (modeEnum == MODE_INDIRECT_DIFFUSE)
-    {
-        color.rgb = indirectDiffuse.SampleLevel(textureSampler, screenUV, 0.0).rgb;
-    }
-    else if (modeEnum == MODE_INDIRECT_SPECULAR)
-    {
-        color.rgb = indirectSpecular.SampleLevel(textureSampler, screenUV, 0.0).rgb;
-    }
-    else if (modeEnum == MODE_VELOCITY_MAP)
-    {
-        float2 vel = velocityMap.SampleLevel(textureSampler, screenUV, 0.0).rg;
-        color.rg = 50.0 * abs(vel);
-    }
+	if (modeEnum == MODE_MATERIAL_ID)
+	{
+		uint id = gbufferData.materialID;
+		float x = float((id * (id + 17) * (id + 15)) & 0xFF) / 255.0;
+		float y = float(((id + 4) * (id + 13) * (id + 11)) & 0xFF) / 255.0;
+		float z = float(((id * 5) * (id + 21) * (id + 19)) & 0xFF) / 255.0;
+		color.rgb = float3(x, y, z);
+	}
+	else if (modeEnum == MODE_ALBEDO)
+	{
+		color.rgb = gbufferData.albedo;
+	}
+	else if (modeEnum == MODE_ROUGHNESS)
+	{
+		color.rgb = gbufferData.roughness.xxx;
+	}
+	else if (modeEnum == MODE_METAL_MASK)
+	{
+		color.rgb = gbufferData.metalMask.xxx;
+	}
+	else if (modeEnum == MODE_NORMAL)
+	{
+		color.rgb = 0.5 * (1.0 + gbufferData.normalWS);
+	}
+	else if (modeEnum == MODE_DIRECT_LIGHTING)
+	{
+		color.rgb = sceneColor.SampleLevel(textureSampler, screenUV, 0.0).rgb;
+	}
+	else if (modeEnum == MODE_RAY_TRACED_SHADOWS)
+	{
+		color.rgb = shadowMask.SampleLevel(textureSampler, screenUV, 0.0).rrr;
+	}
+	else if (modeEnum == MODE_INDIRECT_DIFFUSE)
+	{
+		color.rgb = indirectDiffuse.SampleLevel(textureSampler, screenUV, 0.0).rgb;
+	}
+	else if (modeEnum == MODE_INDIRECT_SPECULAR)
+	{
+		color.rgb = indirectSpecular.SampleLevel(textureSampler, screenUV, 0.0).rgb;
+	}
+	else if (modeEnum == MODE_VELOCITY_MAP)
+	{
+		float2 vel = velocityMap.SampleLevel(textureSampler, screenUV, 0.0).rg;
+		color.rg = 50.0 * abs(vel);
+	}
 	else if (modeEnum == MODE_VISIBILITY_BUFFER)
 	{
 		int2 coord = int2(screenUV * float2(pushConstants.width, pushConstants.height));
@@ -129,9 +129,9 @@ float4 mainPS(Interpolants interpolants) : SV_TARGET
 		color.rgb = float3(R, G, B) / 255.0;
 	}
 
-    // Gamma correction
-    //float gamma = 1.0 / GAMMA_CORRECTION;
-    //color.rgb = pow(color.rgb, float3(gamma, gamma, gamma));
+	// Gamma correction
+	//float gamma = 1.0 / GAMMA_CORRECTION;
+	//color.rgb = pow(color.rgb, float3(gamma, gamma, gamma));
 
 	return color;
 }
