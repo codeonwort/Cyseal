602db7a378131d7d35db9f41153972597403dadc
codeonwort <codeonwort@gmail.com>
2026-04-28 21:32:39 +0900

    Reduce LoCs for pipeline creation

--- projects/Cyseal/src/render/frame_gen_pass.cpp
+++ projects/Cyseal/src/render/frame_gen_pass.cpp
@@ -79,181 +79,31 @@ void FrameGenPass::initializePipelines()
 	frameInterpDescriptor.initialize(L"FSR3_FrameInterpUniform", swapchainCount, sizeof(FrameInterpUniform));
 	inpaintingPyramidDescriptor.initialize(L"FSR3_InpaintingPyramidUniform", swapchainCount, sizeof(InpaintingPyramidUniform));
 
-	// reconstructAndDilatePipeline
+	auto createPipeline = [device = this->device]
+		(const char* debugName, const wchar_t* filepath, UniquePtr<ComputePipelineState>& pipeline)
 	{
-		ShaderStage* shader = device->createShader(EShaderStage::COMPUTE_SHADER, "FSR3ReconstructAndDilateCS");
+		ShaderStage* shader = device->createShader(EShaderStage::COMPUTE_SHADER, debugName);
 		shader->declarePushConstants();
-		shader->loadFromFile(L"amd/ffx_frameinterpolation_reconstruct_and_dilate_pass.hlsl", "CS", { L"FFX_GPU", L"FFX_HLSL", L"FFX_HALF" });
+		shader->loadFromFile(filepath, "CS", { L"FFX_GPU", L"FFX_HLSL", L"FFX_HALF" });
 
-		reconstructAndDilatePipeline = UniquePtr<ComputePipelineState>(device->createComputePipelineState(
-			ComputePipelineDesc{
-				.cs       = shader,
-				.nodeMask = 0,
-			}
+		pipeline = UniquePtr<ComputePipelineState>(device->createComputePipelineState(
+			ComputePipelineDesc{ .cs = shader, .nodeMask = 0 }
 		));
 
 		delete shader;
-	}
+	};
 
-	// setupPipeline
-	{
-		ShaderStage* shader = device->createShader(EShaderStage::COMPUTE_SHADER, "FSR3SetupCS");
-		shader->declarePushConstants();
-		shader->loadFromFile(L"amd/ffx_frameinterpolation_setup_pass.hlsl", "CS", { L"FFX_GPU", L"FFX_HLSL", L"FFX_HALF" });
-
-		setupPipeline = UniquePtr<ComputePipelineState>(device->createComputePipelineState(
-			ComputePipelineDesc{
-				.cs       = shader,
-				.nodeMask = 0,
-			}
-		));
-
-		delete shader;
-	}
-
-	// reconstructPrevDepthPipeline
-	{
-		ShaderStage* shader = device->createShader(EShaderStage::COMPUTE_SHADER, "FSR3ReconstructPrevDepthCS");
-		shader->declarePushConstants();
-		shader->loadFromFile(L"amd/ffx_frameinterpolation_reconstruct_previous_depth_pass.hlsl", "CS", { L"FFX_GPU", L"FFX_HLSL", L"FFX_HALF" });
-
-		reconstructPrevDepthPipeline = UniquePtr<ComputePipelineState>(device->createComputePipelineState(
-			ComputePipelineDesc{
-				.cs       = shader,
-				.nodeMask = 0,
-			}
-		));
-
-		delete shader;
-	}
-
-	// gameMotionVectorFieldPipeline
-	{
-		ShaderStage* shader = device->createShader(EShaderStage::COMPUTE_SHADER, "FSR3GameMotionVectorFieldPipelineCS");
-		shader->declarePushConstants();
-		shader->loadFromFile(L"amd/ffx_frameinterpolation_game_motion_vector_field_pass.hlsl", "CS", { L"FFX_GPU", L"FFX_HLSL", L"FFX_HALF" });
-
-		gameMotionVectorFieldPipeline = UniquePtr<ComputePipelineState>(device->createComputePipelineState(
-			ComputePipelineDesc{
-				.cs       = shader,
-				.nodeMask = 0,
-			}
-		));
-
-		delete shader;
-	}
-
-	// opticalFlowVectorFieldPipeline
-	{
-		ShaderStage* shader = device->createShader(EShaderStage::COMPUTE_SHADER, "FSR3OpticalFlowVectorFieldPipelineCS");
-		shader->declarePushConstants();
-		shader->loadFromFile(L"amd/ffx_frameinterpolation_optical_flow_vector_field_pass.hlsl", "CS", { L"FFX_GPU", L"FFX_HLSL", L"FFX_HALF" });
-
-		opticalFlowVectorFieldPipeline = UniquePtr<ComputePipelineState>(device->createComputePipelineState(
-			ComputePipelineDesc{
-				.cs       = shader,
-				.nodeMask = 0,
-			}
-		));
-
-		delete shader;
-	}
-
-	// disocclusionMaskPipeline
-	{
-		ShaderStage* shader = device->createShader(EShaderStage::COMPUTE_SHADER, "FSR3DisocclusionMaskPipelineCS");
-		shader->declarePushConstants();
-		shader->loadFromFile(L"amd/ffx_frameinterpolation_disocclusion_mask_pass.hlsl", "CS", { L"FFX_GPU", L"FFX_HLSL", L"FFX_HALF" });
-
-		disocclusionMaskPipeline = UniquePtr<ComputePipelineState>(device->createComputePipelineState(
-			ComputePipelineDesc{
-				.cs       = shader,
-				.nodeMask = 0,
-			}
-		));
-
-		delete shader;
-	}
-
-	// interpolationPipeline
-	{
-		ShaderStage* shader = device->createShader(EShaderStage::COMPUTE_SHADER, "FSR3InterpolationPipelineCS");
-		shader->declarePushConstants();
-		shader->loadFromFile(L"amd/ffx_frameinterpolation_pass.hlsl", "CS", { L"FFX_GPU", L"FFX_HLSL", L"FFX_HALF" });
-
-		interpolationPipeline = UniquePtr<ComputePipelineState>(device->createComputePipelineState(
-			ComputePipelineDesc{
-				.cs       = shader,
-				.nodeMask = 0,
-			}
-		));
-
-		delete shader;
-	}
-
-	// inpaintingPyramidPipeline
-	{
-		ShaderStage* shader = device->createShader(EShaderStage::COMPUTE_SHADER, "FSR3InpaintingPyramidPipelineCS");
-		shader->declarePushConstants();
-		shader->loadFromFile(L"amd/ffx_frameinterpolation_compute_inpainting_pyramid_pass.hlsl", "CS", { L"FFX_GPU", L"FFX_HLSL", L"FFX_HALF" });
-
-		inpaintingPyramidPipeline = UniquePtr<ComputePipelineState>(device->createComputePipelineState(
-			ComputePipelineDesc{
-				.cs       = shader,
-				.nodeMask = 0,
-			}
-		));
-
-		delete shader;
-	}
-
-	// inpaintingPipeline
-	{
-		ShaderStage* shader = device->createShader(EShaderStage::COMPUTE_SHADER, "FSR3InpaintingPipelineCS");
-		shader->declarePushConstants();
-		shader->loadFromFile(L"amd/ffx_frameinterpolation_inpainting_pass.hlsl", "CS", { L"FFX_GPU", L"FFX_HLSL", L"FFX_HALF" });
-
-		inpaintingPipeline = UniquePtr<ComputePipelineState>(device->createComputePipelineState(
-			ComputePipelineDesc{
-				.cs       = shader,
-				.nodeMask = 0,
-			}
-		));
-
-		delete shader;
-	}
-
-	// gameVectorFieldInpaintingPyramidPipeline
-	{
-		ShaderStage* shader = device->createShader(EShaderStage::COMPUTE_SHADER, "FSR3GameVectorFieldInpaintingPyramidPipelineCS");
-		shader->declarePushConstants();
-		shader->loadFromFile(L"amd/ffx_frameinterpolation_compute_game_vector_field_inpainting_pyramid_pass.hlsl", "CS", { L"FFX_GPU", L"FFX_HLSL", L"FFX_HALF" });
-
-		gameVectorFieldInpaintingPyramidPipeline = UniquePtr<ComputePipelineState>(device->createComputePipelineState(
-			ComputePipelineDesc{
-				.cs       = shader,
-				.nodeMask = 0,
-			}
-		));
-
-		delete shader;
-	}
-
-	// debugViewPipeline
-	{
-		ShaderStage* shader = device->createShader(EShaderStage::COMPUTE_SHADER, "FSR3DebugViewPipelineCS");
-		shader->declarePushConstants();
-		shader->loadFromFile(L"amd/ffx_frameinterpolation_debug_view_pass.hlsl", "CS", { L"FFX_GPU", L"FFX_HLSL", L"FFX_HALF" });
-
-		debugViewPipeline = UniquePtr<ComputePipelineState>(device->createComputePipelineState(
-			ComputePipelineDesc{
-				.cs       = shader,
-				.nodeMask = 0,
-			}
-		));
-
-		delete shader;
-	}
+	createPipeline("FSR3ReconstructAndDilateCS", L"amd/ffx_frameinterpolation_reconstruct_and_dilate_pass.hlsl", reconstructAndDilatePipeline);
+	createPipeline("FSR3SetupCS", L"amd/ffx_frameinterpolation_setup_pass.hlsl", setupPipeline);
+	createPipeline("FSR3ReconstructPrevDepthCS", L"amd/ffx_frameinterpolation_reconstruct_previous_depth_pass.hlsl", reconstructPrevDepthPipeline);
+	createPipeline("FSR3GameMotionVectorFieldPipelineCS", L"amd/ffx_frameinterpolation_game_motion_vector_field_pass.hlsl", gameMotionVectorFieldPipeline);
+	createPipeline("FSR3OpticalFlowVectorFieldPipelineCS", L"amd/ffx_frameinterpolation_optical_flow_vector_field_pass.hlsl", opticalFlowVectorFieldPipeline);
+	createPipeline("FSR3DisocclusionMaskPipelineCS", L"amd/ffx_frameinterpolation_disocclusion_mask_pass.hlsl", disocclusionMaskPipeline);
+	createPipeline("FSR3InterpolationPipelineCS", L"amd/ffx_frameinterpolation_pass.hlsl", interpolationPipeline);
+	createPipeline("FSR3InpaintingPyramidPipelineCS", L"amd/ffx_frameinterpolation_compute_inpainting_pyramid_pass.hlsl", inpaintingPyramidPipeline);
+	createPipeline("FSR3InpaintingPipelineCS", L"amd/ffx_frameinterpolation_inpainting_pass.hlsl", inpaintingPipeline);
+	createPipeline("FSR3GameVectorFieldInpaintingPyramidPipelineCS", L"amd/ffx_frameinterpolation_compute_game_vector_field_inpainting_pyramid_pass.hlsl", gameVectorFieldInpaintingPyramidPipeline);
+	createPipeline("FSR3DebugViewPipelineCS", L"amd/ffx_frameinterpolation_debug_view_pass.hlsl", debugViewPipeline);
 }
 
 // See ffxFrameInterpolationPrepare.
