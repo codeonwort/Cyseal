79cf2640a7cd3a313280e98d1f96f0e57c96f133
codeonwort <codeonwort@gmail.com>
2026-05-07 00:45:03 +0900

    Support R16G16_UINT

--- projects/Cyseal/src/rhi/dx12/d3d_into.h
+++ projects/Cyseal/src/rhi/dx12/d3d_into.h
@@ -262,6 +262,7 @@ namespace into_d3d
 			case EPixelFormat::R32_UINT                 : return DXGI_FORMAT_R32_UINT;
 			case EPixelFormat::R16_UINT                 : return DXGI_FORMAT_R16_UINT;
 			case EPixelFormat::R8_UINT                  : return DXGI_FORMAT_R8_UINT;
+			case EPixelFormat::R16G16_UINT              : return DXGI_FORMAT_R16G16_UINT;
 			case EPixelFormat::R32G32B32A32_UINT        : return DXGI_FORMAT_R32G32B32A32_UINT;
 			case EPixelFormat::R16G16_SINT              : return DXGI_FORMAT_R16G16_SINT;
 			case EPixelFormat::D24_UNORM_S8_UINT        : return DXGI_FORMAT_D24_UNORM_S8_UINT;
